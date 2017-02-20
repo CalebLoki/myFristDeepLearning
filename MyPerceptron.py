fca39df2787da4e0caa5186758c13d6bb9723900
FOXMAN <foxman@FOXMANdeMacBook-Pro.local>
2017-02-21 02:03:13 +0800

    I'm wanna sleeeeeeep...

--- MyPerceptron.py
+++ MyPerceptron.py
@@ -1,21 +1,25 @@
-
 class MyPerceptron(object):
+    def __init__(self, _initnum_, activation):
+        self.activation = activation
+        self._initnum_ = _initnum_
 
-	def __init__(self,_initnum_,activation):
-		print(_initnum_)
-		self.activation = activation
-	
-	def __str__(self):
-		print("_initnum_ = %f" % (self._initnum_))
+    def __str__(self):
+        return "_initnum_ = %s" % (self._initnum_)
+
+    def doSomething(self, _initnum_):
+        return self.activation(_initnum_)
+
 
 def f(i):
-	return int(i)%2
+    return int(i) % 2
+
 
 def _train_add_():
-	p = MyPerceptron(2,f)
-	return p
+    p = MyPerceptron(2, f)
+    return p
+
 
 if __name__ == "__main__":
-	__perceptron_add__ = _train_add_()
-	print('result = %s'%(__perceptron_add__))
-
+    _perceptron_add_ = _train_add_()
+    print(_perceptron_add_)
+    print('result = %s' % (_perceptron_add_.doSomething(233)))
