44cb0de19ab041d0d4f7b47c43f1524c56d55dac
CalebLoki <Caleb_Loki@hotmail.com>
2017-02-21 13:26:39 +0800

    dosomething

--- MyPerceptron.py
+++ MyPerceptron.py
@@ -7,9 +7,13 @@ class MyPerceptron(object):
 	
 	def __str__(self):
 		return ("_initnum_ = %s" % (self._initnum_))
-		
+	'''	
 	def doSomething(self,_input_num_):
 		return (self.activation(_input_num_+int(self._initnum_)) == 0 and 'Ace') or 'Bob'
+	
+	'''
+	def doSomething(self,_input_num_, str_0 = 'Ace', str_1 = 'Bob'):
+		return str_0 if self.activation(_input_num_+int(self._initnum_)) == 0 else str_1
 
 def f(i):
 	return int(i)%2
@@ -22,4 +26,5 @@ if __name__ == "__main__":
 	__perceptron_add__ = _train_add_()
 	print(__perceptron_add__)
 	print('result = %s' % (__perceptron_add__.doSomething(233)))
+	print('result = %s' % (__perceptron_add__.doSomething(233,'HAHA','HEIHEI')))
 
